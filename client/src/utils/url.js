94b4ed00512ecd71c30b442312bb59993995ffe2
soham haldar <sohamhaldar25@gmail.com>
2024-05-16 12:36:21 +0530

    api url changed

--- client/src/utils/url.js
+++ client/src/utils/url.js
@@ -1,3 +1,3 @@
-const url='http://localhost:4000';
-// const url='https://form-yiml.onrender.com'
+// const url='http://localhost:4000';
+const url='https://form-yiml.onrender.com'
 export default url;
